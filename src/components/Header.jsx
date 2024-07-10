3a2471c6db61ace7ec694510b7a4639fa96008c3
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-10 12:53:52 +0200

    Added Header with temporary logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function  Header() {
+  return (
+    <header>
+      <a href="/">
+        <img src="/logo.png" alt="Logo HTML5" id="logo" className="header__logo"/>
+      </a>
+      <p className="header__text">Zadanie <strong>rekrutacyjne</strong></p>
+    </header>
+  )
+}
+
