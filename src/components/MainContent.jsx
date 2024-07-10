a73e201bd3e4591a7228634b97027c12e3a35fa2
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-10 18:59:11 +0200

    Adding content to MainContent component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,7 +2,21 @@
 export default function MainContent() {
   return (
     <main>
-      
+      <div className="main__header">
+        <h1 className="main__text">Nagłówek H1</h1>
+        <hr className="main__hr"/>
+      </div>
+      <div className="main__blocks">
+        <div className="main__blocks-block1">
+          <h2>Blok pierwszy</h2>
+        </div>
+        <div className="main__blocks-block2">
+          <h2>Blok drugi</h2>
+        </div>
+        <div className="main__blocks-block3">
+          <h2>Blok z długą nazwą która sama się przytnie ...</h2>
+        </div>
+      </div>
     </main>
   )
 }
