dd8c2c42531c722d86e1b7b2fe0c60e9629a2461
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-11 18:51:08 +0200

    Modified RadioButtonForm to reset it with a button in footer

--- src/components/RadioButtonForm.jsx
+++ src/components/RadioButtonForm.jsx
@@ -1,4 +1,4 @@
-export default function RadioButtonForm({ handleRadioChange }) {
+export default function RadioButtonForm({ handleRadioChange, selectedOption }) {
   return (
     <div className="main__blocks-block1-form" role="radiogroup">
       <label className="radio-container" htmlFor="option1" id="labelOption1">
@@ -7,6 +7,7 @@ export default function RadioButtonForm({ handleRadioChange }) {
           id="option1"
           name="options"
           value="1"
+          checked={selectedOption === "1"}
           onChange={handleRadioChange}
           aria-labelledby="labelOption1"
           tabIndex="0"
@@ -20,6 +21,7 @@ export default function RadioButtonForm({ handleRadioChange }) {
           id="option2"
           name="options"
           value="2"
+          checked={selectedOption === "2"}
           onChange={handleRadioChange}
           aria-labelledby="labelOption2"
           tabIndex="0"
@@ -33,6 +35,7 @@ export default function RadioButtonForm({ handleRadioChange }) {
           id="option3"
           name="options"
           value="random"
+          checked={selectedOption === "random"}
           onChange={handleRadioChange}
           aria-labelledby="labelOption3"
           tabIndex="0"
